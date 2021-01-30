2c9a642598a0133014008117b7a24ce14cb0b7b8
Sidharth Soman <siddu.soman65@gmail.com>
2021-01-30 17:23:29 +0530

    Home page nearly done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-console.log("Test")
+const leftBtn=document.querySelector('#leftBtn').addEventListener('click', prevPic);
+const rightBtn=document.querySelector('#rightBtn').addEventListener('click', nextPic);
+const pic=document.querySelector('.cod1');
+console.log(pic);
+const keepPicLinks=["images/codmobilecarousel.jpg", "images/valorantcarousel.jpg", "images/codmobilecarousel2.jpg"];
+let count=0;
 
+function prevPic(){
+	if(!count) count=2;
+	else count--;
+	pic.src=keepPicLinks[count];
+}
+
+function nextPic(){
+	if(count==2) count=0;
+	else count++;
+	pic.src=keepPicLinks[count];
+}
 
 
 
